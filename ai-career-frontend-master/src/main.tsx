da2540995c69fce95d27c3b36909d2db450f9ba2
Krish-Prasad09 <prasadkrish9306@gmail.com>
2026-06-09 21:58:33 +0530

    Added frontend and backend

---
+++ ai-career-frontend-master/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AppProvider } from "./context/AppContext.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+export const server = "http://localhost:5000";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppProvider>
+      <GoogleOAuthProvider clientId="394558418883-hc26qlpp0cnc37ud5i60mremkqm946qk.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    </AppProvider>
+  </StrictMode>
+);
